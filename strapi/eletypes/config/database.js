1e9c77faebefcf2c2044c89af5046909ea9f4106
lele94218 <terry.trxue@gmail.com>
2022-08-31 01:24:17 -0700

    make it support strapi

--- strapi/eletypes/config/database.js
+++ strapi/eletypes/config/database.js
@@ -1,11 +1,16 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+module.exports = ({env}) => ({
+  connection : {
+    client : env('DATABASE_CLIENT', 'postgres'),
+
+    connection : {
+      host : env('DATABASE_HOST', '127.0.0.1'),
+      port : env.int('DATABASE_PORT', 3306),
+      database : env('DATABASE_NAME', 'eletypes'),
+      user : env('DATABASE_USERNAME', 'eletypes'),
+      password : env('DATABASE_PASSWORD', 'eletypespassword'),
     },
-    useNullAsDefault: true,
+    debug : false,
   },
 });
