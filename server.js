548e5e3320acd690c638663832781ef32edff588
rendi12345678 <hardleberg@gmail.com>
2023-11-20 17:14:03 +0700

    Updated server

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require("express")
-const app = express()
-const port = 6000;
+const express = require("express");
+const app = express();
+const cors = require("cors")
+const port = 8000;
 
-app.use(express.json()); 
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cors({
+  origin: '*',
+  crendentials: true
+}));
+
+app.post("/api/login", (req, res) => {
+  const {email, password} = req.body;
+
+  res.status(200).json({ success: true });
+});
 
 app.listen(port, () => {
-    console.log("Server is running on port", port)
-})
+  console.log("Server is running on port", port);
+});
